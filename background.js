13b87d910b652e57b9b35630243da6c840461189
Marcio (PRO) <marcio.reis1@hotmail.com>
2024-08-31 08:43:47 +0200

    initial setup

---
+++ background.js
@@ -0,0 +1,40 @@
+// background.js
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    mainWindow.loadURL(
+        process.env.NODE_ENV === 'development'
+            ? 'http://localhost:8080'
+            : `file://${path.join(__dirname, 'dist/index.html')}`
+    )
+
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools()
+    }
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
