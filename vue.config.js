78710ad261d74a7a8ac4e9048bfb549fe1b05df7
Marcio (PRO) <marcio.reis1@hotmail.com>
2024-08-31 08:46:48 +0200

    add a preload.js file and a updated vue.config.js to with pluginOptions for electronBuilder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      preload: 'preload.js',
+      builderOptions: {
+        appId: 'com.myelectron.vueapp',
+        productName: 'MyElectronVueApp',
+        win: {
+          target: [
+            'nsis'
+          ]
+        },
+        linux: {
+          target: [
+            'AppImage'
+          ]
+        },
+        mac: {
+          target: [
+            'dmg'
+          ]
+        }
+      }
+    }
+  }
+
 })
